dc2c92f121b9683c897e29cd3b7886e4101f275b
K2421 <viivi_2@hotmail.com>
2016-02-10 11:03:23 +0200

    initial

---
+++ PersonApplication/PersonRegister.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{   
+    /// <summary>
+    /// This class hold person objects in a collection
+    /// </summary>
+    /// 
+    class PersonRegister
+    {
+        /// Person List stores person objects...
+
+        private List<Person> persons;
+
+        /// <summary>
+        /// Default constructor initializes a collection
+        /// </summary>
+        /// 
+        public PersonRegister()
+        {
+            persons = new List<Person>();
+        }
+
+        /// <summary>
+        /// Adds a new person to collection
+        /// </summary>
+        /// <param name="person">person to add</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+        /// <summary>
+        /// Returns a person to caller application
+        /// </summary>
+        /// <param name="index">person index in collection</param>
+        /// <returns>person object</returns>
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count)
+            {
+                return persons.ElementAt(index);
+            }else
+            {
+                return null;
+            }
+        }
+        public Person FindPerson(string socialSecurityNumber)
+        {
+            foreach(Person person in persons)
+            {
+                //is sotus same...
+                if (socialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person; // person found with that sotu
+                }
+            }
+            return null; // person with searched sotu not found
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine(" Täällä on kamuja jee: ");
+            Console.WriteLine("***********************");
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
---
+++ PersonApplication/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a register
+            PersonRegister myFriends = new PersonRegister();
+
+            //create a few persons
+            Person person1 = new Person();
+            person1.Firstname = "Matti";
+            person1.Lastname = "Mainio";
+            person1.SocialSecurityNumber = " 122122-1212";
+
+            Person person2 = new Person { Firstname = "Kirsi", Lastname = "Kernel", SocialSecurityNumber = "111111-2222" };
+
+            // add persons to collection
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+
+            // get on person from register
+            Person person3 = myFriends.GetPerson(0); // index = 0
+            if (person3 != null)
+            {
+                Console.WriteLine(person3.ToString());
+            } else
+            {
+                Console.WriteLine("Can't find a person in that index!");
+            }
+
+            Console.WriteLine();
+
+            string sotu = "111111-2422";
+            Console.WriteLine("Finding person with sotu : " + sotu);
+            Person person4 = myFriends.FindPerson(sotu);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }else
+            {
+                Console.WriteLine("Can't find person with that sotu!");
+            }
+            Console.WriteLine();
+            myFriends.PrintData();
+            Console.WriteLine();
+        }
+
+       
+    }
+}
